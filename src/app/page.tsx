728c67cd82e18cc2eae5d3bfe81583118655ea7e
sandrbuz <sandrbuz@mail.ru>
2025-04-21 03:32:42 +0300

    Установил конфиг airbnb для eslint с помощью cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Home() {
   return (
     <>
       <div>fdsf</div>
+      <div>fsdfsd</div>
     </>
   );
 }
